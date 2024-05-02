dde62ef8597e140cab5b20cd97caee785f59cb52
Kevin Yao <kevinyao2003@hotmail.com>
2024-05-03 02:41:48 +1200

    adjustments to modal

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
 // get modal
 var modal = document.getElementById('popup-menu');
 
-//get button
-var main_btn = document.getElementById('trigger-modal');
-var nav_btn = document.getElementById('trigger-modal2');
 
-
-//get close button
 var close = document.getElementsByClassName('close')[0];
 
 // On button click, open modal
-main_btn.onclick = function() {
-  modal.style.display = 'block';
-} 
-nav_btn.onclick = function() {
+document.querySelectorAll('.trigger-modal').forEach(element => {
+  element.addEventListener('click', function() {
     modal.style.display = 'block';
-  } 
+  });
+});
+
 // On close button click, close modal
 close.onclick = function() {
   modal.style.display = 'none';
 }
 
+// also closes menu when clicking outside the modal
 window.onclick = function(event) { 
     if (event.target == modal) {
         modal.style.display = 'none';
